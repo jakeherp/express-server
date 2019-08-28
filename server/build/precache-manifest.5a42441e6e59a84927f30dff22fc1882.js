63cbff2234f1d69b5b7e56425010f38b94d3d410
Jacob Herper <jacob@herper.uk>
2019-08-28 14:54:29 +0100

    Production build added to server

---
+++ server/build/precache-manifest.5a42441e6e59a84927f30dff22fc1882.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fea8f6c223ddee8c311d6a227a24fd16",
+    "url": "/index.html"
+  },
+  {
+    "revision": "964b96c25c1c9567e525",
+    "url": "/static/js/2.7b493bae.chunk.js"
+  },
+  {
+    "revision": "2eeddea59196c8bf234c",
+    "url": "/static/js/main.0216633d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
